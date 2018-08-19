c9bbfb78656fc3d9d068fb34b83221a78a7c8748
Andre Farzat <andrefarzat@gmail.com>
2018-08-19 17:03:01 -0300

    Returning to simple constructed

--- src/localsearch/Constructed_RRLS.ts
+++ src/localsearch/Constructed_RRLS.ts
@@ -11,7 +11,44 @@ import * as path from 'path';
  * @see https://www.oreilly.com/learning/regex-golf-with-peter-norvig
  */
 export default class Constructed_RRLS extends RRLS {
+
     public generateInitialIndividual(): Individual {
+        let index = 0;
+        let maxIndex = this.validLeftChars.length - 1;
+
+        let ind = new Individual();
+        ind.tree = new Func();
+        ind.tree.type = Func.Types.concatenation;
+        ind.tree.left = new Terminal(this.validLeftChars[index]);
+        ind.tree.right = new Terminal('');
+
+        if (this.isValidLeft(ind)) {
+            return ind;
+        }
+
+        let current = ind.tree;
+        while (!this.isValidLeft(ind)) {
+            current.type = Func.Types.or;
+            index ++;
+
+            if ((index + 1) > maxIndex) {
+                break;
+            }
+
+            let func = new Func();
+            func.type = Func.Types.or;
+            func.left = new Terminal(this.validLeftChars[index]);
+            func.right = new Terminal(this.validLeftChars[index + 1]);
+
+            current.right = func;
+            current = func;
+        }
+
+        return ind;
+    }
+
+
+    public NORVIG_generateInitialIndividual(): Individual {
         let cmd = path.join('norvig', 'regexgolf.py');
         let norvigResult = cp.execSync(`${cmd} ${this.instanceName}`);
         let str = eval(norvigResult.toString());
